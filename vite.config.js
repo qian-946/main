36ca1b926b998ce7102bb0dfb3a010d0515ac7da
qianyujie3-kdgc <xqian.yujie3@contract.dentsusoken.com>
2026-09-23 10:38:05 +0800

    vite.config 配置修改

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+// 在 ESM 模块中正确获取当前文件的路径信息
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [
